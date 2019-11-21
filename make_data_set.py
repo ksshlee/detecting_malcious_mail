ce26f562522e54aad3e7912c12992f003df9897f
ksshlee <ksshlee@naver.com>
2019-11-22 01:59:24 +0900

    make_data_set모듈에서 none이면 예외처리 코드 추가

--- make_data_set.py
+++ make_data_set.py
@@ -1,16 +1,35 @@
 import whois
 
 f = open('data.txt','r')
-a = open('data_set.txt','a') # save a data
-p = open('passeddata.txt','a') #save a passed data
+a = open('ohoho.txt','a') # save a data
+p = open('oohohpass.txt','a') #save a passed data
 
 data= f.read().splitlines() 
 for rst in data:
     print(rst)
     try:
         ans = whois.whois('{0}'.format(rst))
-        # if ('None' in ans['domain_name'] or 'None' in ans['creation_date']):
-        #     raise
+        #도메인 이름이나 생성날짜가 none일때 예외 발생
+
+        #도메인 이름이 list형태일때
+        domain_name = ans.get('domain_name')
+        creation_date = ans.get('creation_date')
+        if(isinstance(domain_name,list)):
+            for i in domain_name:
+                if i is None:#domain이름이 none일때 예외 발생
+                    raise
+        else:
+            if domain_name is None:
+                raise
+        
+        #생성날짜가 list 형태일때
+        if(isinstance(creation_date,list)):
+            for i in creation_date:
+                if i is None:#생성날짜가 none일때 예외 발생
+                    raise
+        else:
+            if creation_date is None:
+                raise
 
         a.write(rst+" : ")
         if type(ans['domain_name']) is list:
